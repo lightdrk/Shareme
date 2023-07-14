aa574d91c33f08aa5eb5ee3266ff73fa790907a8
Mohit <mohitrawatssj4@gmail.com>
2023-07-14 06:57:27 +0530

    update

---
+++ backend/connect.js
@@ -0,0 +1,3 @@
+const ftp = require('basic-ftp')
+
+
---
+++ script.js
@@ -0,0 +1,18 @@
+const connectEvent = document.getElementById('connectId');
+const cloudsEvent = document.getElementById('CloudsId');
+const filesEvent = document.getElementById('FilesId');
+const workspaceEvent = document.getElementById('WorkspaceId');
+const logoutEvent = document.getElementById('logout');
+const changeInP = document.getElementById('changeIt');
+
+connectEvent.innerText
+function WorkspaceEvent(){
+    changeInP.innerText = "WORKSPACE";
+    
+
+}
+
+
+workspaceEvent.addEventListener('click',WorkspaceEvent());
+//workspaceEvent.addEventListener("click",WorkspaceEvent);
+
