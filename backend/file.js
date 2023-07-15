b1880a5871dc047d1d493b94dcaaa246c13d72eb
Mohit <mohitrawatssj4@gmail.com>
2023-07-15 06:11:07 +0530

    update

--- backend/file.js
+++ backend/file.js
@@ -1,4 +1,26 @@
 var fs = require('fs');
 
-const WorkspacePath = process.cwd();
-fs.readFileSync
+const cwd = process.cwd();
+var WorkspacePath = "/home/arch/Desktop/TestFolder/test";
+//get Path from frontend
+function PathFiles(){
+    //here call for data from front end 
+    //front end will as confirm path like are you sure naming folder etc.
+    try{
+        if(!fs.existsSync(WorkspacePath)){
+            fs.mkdir(WorkspacePath);
+        }
+    } catch(err){
+        console.error(err);
+    }
+    //return WorkspacePath;
+}
+
+function dirContent(){
+    // WorkspacePath = function PathFiles();
+    PathFiles();
+    console.log(fs.readdirSync(WorkspacePath));
+}
+
+
+dirContent();
--- script.js
+++ script.js
@@ -6,13 +6,32 @@ const logoutEvent = document.getElementById('logout');
 const changeInP = document.getElementById('changeIt');
 
 connectEvent.innerText
+//workspace funtions 
 function WorkspaceEvent(){
     changeInP.innerText = "WORKSPACE";
-    
-
 }
 
+// connect options funtions
 
-workspaceEvent.addEventListener('click',WorkspaceEvent());
+function ConnectEvent(){
+    changeInP.innerText = "CONNECT";
+}
+
+function CloudsEvent(){
+    changeInP.innerText = "CLOUD";
+}
+
+function FilesEvent(){
+    changeInP.innerText = "FILES";
+}
+
+function LgoutEvent(){
+
+}
+workspaceEvent.addEventListener('click',WorkspaceEvent);
 //workspaceEvent.addEventListener("click",WorkspaceEvent);
+connectEvent.addEventListener('click',ConnectEvent);
 
+cloudsEvent.addEventListener('click',CloudsEvent);
+
+filesEvent.addEventListener('click',FilesEvent);
