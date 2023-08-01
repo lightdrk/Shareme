17675d0f557b9e62e41c3a2372db6def97d749ad
Mohit <mohitrawatssj4@gmail.com>
2023-08-02 00:59:26 +0530

    update

--- backend/envM.js
+++ backend/envM.js
@@ -2,25 +2,29 @@ const fs = require('fs')
 const ips = require('./ip')
 const password = require('./password')
 
-ips.getPublicIP((err,ip_json)=>{
-    if (err){
-        console.error(err);
-    }else{
-        ips.getLocalIP((err,localip)=>{
-            if(err){
-                console.error(err);
-            }else{
-                console.log(localip)
-            }
-        });
-        let value = password.pass();
-        fs.writeFile('cred.env',`PublicIP = $(ip_json)\n LocalIP = $(localip)\nPort = 8080\nUser = fptuser\nPWD = $(value) `, function(err){
-            if (err) throw err;
+function Menv(){
+    ips.getPublicIP((err,ip_json)=>{
+        if (err){
+            console.error(err);
+        }else{
+            console.log(ip_json);
+            ips.getLocalIP((err,localip)=>{
+                if(err){
+                    console.error(err);
+                }else{
+                    console.log(localip);
+                    let value = password.pass();
+                    fs.writeFile('.env',`PublicIP = ${ip_json['ip']}\nLocalIP = ${localip}\nPort = 8080\nUser = fptuser\nPWD = ${value} `, function(err){
+                        if (err) throw err;
+                        console.log('.env created!!!!!')
+                    });
+                }
+            });
+            
         }
-        );
-    }
-});
+    });
+}
 
+Menv()
 
-
-console.log(value);
+exports.Menv = Menv
--- backend/ftpServer.js
+++ backend/ftpServer.js
@@ -1,17 +1,20 @@
 const ftpd = require('ftpd')
 const fs = require('fs')
-const path = require('path')
+const path = require('path');
+const { error } = require('console');
+const { exit } = require('process');
 
 require('dotenv').config()
 
-var keyFile
-var certFile
-var server
+var keyFile;
+var certFile;
+var passpharse;
+var server;
 
 var options = {
     LocalHost: process.env.LocalIP || '127.0.0.1',
     Host: process.env.PublicIP,
-    port: process.env.Port || 80,
+    Port: process.env.Port || 80,
     tls: null,
 }
 // check and under stand 
@@ -30,13 +33,77 @@ if (keyFile && certFile){
     console.log('ssl  err ,cert err')
 }
 
-server = new ftpd.FtpServer(options.Host,{
-    //getting shared file path from front end
-    getInitialCwd: function(){
-        return '/ftproot' //change needed
+server = new ftpd.FtpServer(options.LocalHost,{
+    //getting shared folder path from front end
+    getInitialCwd: function(sharePath = '/home/arch/Desktop',callback){
+        if(fs.existsSync(sharePath)){
+            console.log(sharePath)
+            return sharePath;
+        }else{
+            exit('path not found');
+        }
     },
     getRoot: function(){
-        return process.cwd()
+        return process.cwd();
     },
-    // check further and understand 
-})
+    pasvPortRangeStart: 1025,
+    pasvPortRangeEnd: 1050,
+    tlsOptions: options.tls,
+    allowUnauthorizedTls: false,
+    useReadFile: true,
+    useWriteFile: false,
+    allowedCommands: [ 
+    'XMKD',
+    'AUTH',
+    'TLS',
+    'SSL',
+    'USER',
+    'PASS',
+    'PWD',
+    'OPTS',
+    'TYPE',
+    'PORT',
+    'PASV',
+    'LIST',
+    'CWD',
+    'MKD',
+    'SIZE',
+    'STOR',
+    'MDTM',
+    'DELE',
+    'QUIT'
+    ],
+
+});
+
+server.on('error',function(error){
+    console.log('FTP Server error:',error);
+});
+
+server.on('client:connected', function(connection){
+    console.log('client connected: '+connection.remoteAddress);
+    connection.on('command:user',function(user,success,failure){
+        console.log(user);
+        if (user){
+            success();
+        }else{
+            failure();
+        }
+    })
+    connection.on('command:pass',function(pass,success,failure){
+        console.log('!!!!!!!!!!' + process.env.Pass);
+        console.log('#####');
+        console.log(process.env.Pass == pass);
+        console.log('###########');
+        if(process.env.Pass == pass){
+            console.log('success',pass);
+            success();
+        }else{
+            failure();
+        }
+    })
+});
+
+server.debugging = 4;
+server.listen(options.Port);
+console.log('port :' + options.Port);
