abb08b678307946442fb9e5fac03cc099b5c55a7
Mohit <mohitrawatssj4@gmail.com>
2023-08-03 00:07:16 +0530

    update

---
+++ backend/test.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+
+
+console.log(fs.existsSync('/home/arch/Desktop'))
+
+allowedCommands: [ 
+    'XMKD',
+    'AUTH',
+    'TLS',
+    'SSL',
+    'USER',
+    'PASS',
+    'PWD',
+    'OPTS',
+    'TYPE',
+    'PORT',
+    'PASV',
+    'LIST',
+    'CWD',
+    'MKD',
+    'SIZE',
+    'STOR',
+    'MDTM',
+    'DELE',
+    'QUIT'
+    ],
+
+
+    
